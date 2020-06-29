c570ccbb89f2c475722cc58576a6879c48cb26ad
thapatechnical <vbthapa55@gmail.com>
2020-06-29 16:36:50 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Bootstrap 4 Example</title>
+		<?php  include 'links/link.php'  ?>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+	<body>
+	
+					
+					
+					<?php  include 'menu.php' ?>
+					<!-- ======= Hero Section ======= -->
+					<section id="hero" class="d-flex align-items-center  hero_main_margin">
+						<div class="container-fluid">
+							<div class="row">
+								<div class="col-10 mx-auto">
+									<div class="row">
+										
+										
+										<div class="col-lg-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center main_header_content">
+											<h1 >Beautiful websites, creative technology,real results.
+											</h1>
+											<h2 >We are the team of Creative Developer.</h2>
+											<div>
+												<div class="services hero_main_btn">
+													<a href="#services" class="btn-get-started " > Check More
+													</a>
+												</div>
+											</div>
+										</div>
+										<div class="col-lg-6 order-1 order-lg-2 hero-img " >
+											<figure class="text-center">
+												<img src="images/heroslider/hs1.svg" class="img-fluid animations"  alt="">
+											</figure>
+											
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						</section><!-- End Hero -->
+		</body>
+	</html>
---
+++ menu.php
@@ -0,0 +1,45 @@
+<div>
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-10 mx-auto">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand " href="#">NB WebSolution</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav w-100 d-flex justify-content-center align-items-end " id="navbar_id">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Services</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="#">Portfolio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="#">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="#">Contact</a>
+            </li>
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            <button class="btn btn-outline-success my-2 my-sm-0" >9518365545</button>
+          </form>
+        </div>
+      </nav>
+    </div>
+  </div>
+</div>
+</div>
+<script type="text/javascript">
+if ($(window).width() < 992) {
+$('#navbar_id').removeClass('w-100');
+$('#navbar_id').removeClass('d-flex');
+$('#navbar_id').removeClass('align-items-end');
+} else {
+$('#navbar_id').addClass('w-100');
+}
+</script>
