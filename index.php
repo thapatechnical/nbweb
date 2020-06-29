c27f64a6102ab5731bd13781378aedadb55e3c1b
thapatechnical <vbthapa55@gmail.com>
2020-06-29 17:45:35 +0530

    adding services section

--- index.php
+++ index.php
@@ -6,39 +6,185 @@
 		<link rel="stylesheet" type="text/css" href="css/style.css">
 	</head>
 	<body>
-	
-					
-					
-					<?php  include 'menu.php' ?>
-					<!-- ======= Hero Section ======= -->
-					<section id="hero" class="d-flex align-items-center  hero_main_margin">
-						<div class="container-fluid">
-							<div class="row">
-								<div class="col-10 mx-auto">
-									<div class="row">
-										
-										
-										<div class="col-lg-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center main_header_content">
-											<h1 >Beautiful websites, creative technology,real results.
-											</h1>
-											<h2 >We are the team of Creative Developer.</h2>
-											<div>
-												<div class="services hero_main_btn">
-													<a href="#services" class="btn-get-started " > Check More
-													</a>
-												</div>
-											</div>
-										</div>
-										<div class="col-lg-6 order-1 order-lg-2 hero-img " >
-											<figure class="text-center">
-												<img src="images/heroslider/hs1.svg" class="img-fluid animations"  alt="">
-											</figure>
-											
-										</div>
+		
+		
+		
+		<?php  include 'menu.php' ?>
+		<!-- ======= Hero Section ======= -->
+		<section id="hero" class="d-flex align-items-center  hero_main_margin">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-10 mx-auto">
+						<div class="row">
+							
+							
+							<div class="col-lg-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center main_header_content">
+								<h1 >Beautiful websites, creative technology,real results.
+								</h1>
+								<h2 >We are the team of Creative Developer.</h2>
+								<div>
+									<div class="services hero_main_btn">
+										<a href="#services" class="btn-get-started " > Check More
+										</a>
 									</div>
 								</div>
 							</div>
+							<div class="col-lg-6 order-1 order-lg-2 hero-img " >
+								<figure class="text-center">
+									<img src="images/heroslider/hs1.svg" class="img-fluid animations"  alt="">
+								</figure>
+								
+							</div>
 						</div>
-						</section><!-- End Hero -->
+					</div>
+				</div>
+			</div>
+			</section><!-- End Hero -->
+
+			<section class="Common_section">
+				<div>
+					<h1 class="text-center">What We Do</h1>
+				</div>
+				
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-10 mx-auto">
+							
+							<div class="row common-padding">
+								
+								<div class="col-lg-4 col-sm-6" id="b">
+									<a href="web-design"   class="">
+										<div class="hvr-wobble-bottom">
+											<div class="row wwtab ">
+												<div class="col-3 wwiconcol">
+													<figure>
+														<img src="https://excellone.co.uk/wp-content/uploads/2020/02/web.png" alt="icon">
+													</figure>
+												</div>
+												<div class="col-9">
+													<h6><span>Bespoke Web Design</span></h6>
+													<p>Excellone Technologies provide both web	</p>
+													<p class="ww-readmore">
+														Read More
+													</p>
+													
+												</div>
+											</div>
+										</div>
+									</a>
+								</div>
+								
+								<div class="col-lg-4 col-sm-6" id="b">
+									<a href="ecommerce"   class="">
+										<div class="hvr-wobble-bottom">
+											<div class="row wwtab ">
+												<div class="col-3 wwiconcol">
+													<figure>
+														<img src="https://excellone.co.uk/wp-content/uploads/2020/02/ecommerce.png" alt="icon">
+													</figure>
+												</div>
+												<div class="col-9">
+													<h6><span>Ecommerce Websites</span></h6>
+													<p>Excellone Technologies provide both web	</p>
+													<p class="ww-readmore">
+														Read More
+													</p>
+													
+												</div>
+											</div>
+										</div>
+									</a>
+								</div>
+								
+								<div class="col-lg-4 col-sm-6" id="b">
+									<a href="branding"   class="">
+										<div class="hvr-wobble-bottom">
+											<div class="row wwtab ">
+												<div class="col-3 wwiconcol">
+													<figure>
+														<img src="https://excellone.co.uk/wp-content/uploads/2020/02/branding.png" alt="icon">
+													</figure>
+												</div>
+												<div class="col-9">
+													<h6><span>Branding</span></h6>
+													<p>Excellone Technologies provide both web	</p>
+													<p class="ww-readmore">
+														Read More
+													</p>
+													
+												</div>
+											</div>
+										</div>
+									</a>
+								</div>
+								
+								<div class="col-lg-4 col-sm-6" id="b">
+									<a href="mobile-apps"   class="">
+										<div class="hvr-wobble-bottom">
+											<div class="row wwtab ">
+												<div class="col-3 wwiconcol">
+													<figure>
+														<img src="https://excellone.co.uk/wp-content/uploads/2020/02/mobileapp.png" alt="icon">
+													</figure>
+												</div>
+												<div class="col-9">
+													<h6><span>Mobile Apps</span></h6>
+													<p>Excellone Technologies provide both web	</p>								<p class="ww-readmore">
+														Read More
+													</p>
+													
+												</div>
+											</div>
+										</div>
+									</a>
+								</div>
+								
+								<div class="col-lg-4 col-sm-6" id="b">
+									<a href="web-based-applications"   class="">
+										<div class="hvr-wobble-bottom">
+											<div class="row wwtab ">
+												<div class="col-3 wwiconcol">
+													<figure>
+														<img src="https://excellone.co.uk/wp-content/uploads/2020/02/software.png" alt="icon">
+													</figure>
+												</div>
+												<div class="col-9">
+													<h6><span>Web based applications</span></h6>
+													<p>Excellone Technologies provide both web	</p>										<p class="ww-readmore">
+														Read More
+													</p>
+													
+												</div>
+											</div>
+										</div>
+									</a>
+								</div>
+								
+								<div class="col-lg-4 col-sm-6" id="b">
+									<a href="graphic-design"   class="">
+										<div class="hvr-wobble-bottom">
+											<div class="row wwtab ">
+												<div class="col-3 wwiconcol">
+													<figure>
+														<img src="https://excellone.co.uk/wp-content/uploads/2020/02/graphic-design-1.png" alt="icon">
+													</figure>
+												</div>
+												<div class="col-9">
+													<h6><span>Graphic Design</span></h6>
+												<p>Excellone Technologies provide both web	</p>			<p class="ww-readmore">
+														Read More
+													</p>
+													
+												</div>
+											</div>
+										</div>
+									</a>
+								</div>
+							</div>
+							
+						</div>
+					</div>
+				</div>
+			</section>
 		</body>
 	</html>
